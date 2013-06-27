6ce9d439fae4f031afc1cf14ead754fea2898015
Tommy Palmer <hi@tommyp.org>
2013-06-27 14:12:08 +0100

    new approach on primes to calculate n primes, not primes up to n

--- week1/primes/primes.js
+++ week1/primes/primes.js
@@ -2,22 +2,33 @@
 var fs = require('fs');
 var outfile = "primes.txt";
 
-function sieve(n, limit) {
-  var primes = [], sieve = [], i, j;
 
-  for (i = n; i <= limit; ++i) {
-    if (!sieve[i]) {
-      primes.push(i)
-      for (j = i << 1; j <= limit; j += i) {
-        sieve[j] = true;
+
+
+function find_primes(start, limit) {
+  var primes = [], i, j;
+  
+  for (i = start; primes.length <= limit; i++) {
+    for (j = start; j <= Math.sqrt(i); j++) {
+      var result, top, bottom;
+      result = i/j
+      top = Math.ceil(result)
+      bottom = Math.floor(result)
+      console.log(i + ' Result: ' + result + ' Floor: ' + bottom + ' Top: ' + top)
+      if (result === top && result === bottom ) {
+        console.log(i + ' not a prime')
+      } else {
+        console.log(i + ' is a prime')
+        primes.push(i);
       }
     }
   }
+
   return primes;
 }
 
 
 
 
-fs.writeFileSync(outfile, sieve(2, 100).join(','));  
+fs.writeFileSync(outfile, find_primes(2, 100).join(','));  
 console.log("Script: " + __filename + "\nWrote: To: " + outfile);
